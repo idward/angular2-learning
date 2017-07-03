bf2750876118337a564b6ff01c974a9f323b5c18
idwardluo <apple@idwardluo.local>
2017-07-03 11:25:24 +0800

    update input-output

--- dev/input-output/child.component.ts
+++ dev/input-output/child.component.ts
@@ -1,4 +1,4 @@
-import {Component, Output} from 'angular2/core';
+import {Component, Output, Input} from 'angular2/core';
 import {EventEmitter} from 'angular2/core';
 
 @Component({
@@ -10,12 +10,13 @@ import {EventEmitter} from 'angular2/core';
             <input type="text" #childInput value="" (keyup)="onChange(childInput.value)">
         </div>
     `,
-  inputs:['parentValue:passedValue'],
+  //inputs:['parentValue:passedValue'],
   //outputs:['childValueChanged']
 })
 
 export class ChildComponent {
-  @Output() public childValueChanged = new EventEmitter<string>();
+  @Input() 'parentValue:passedValue';
+  @Output() childValueChanged = new EventEmitter<string>();
 
   onChange(value:string){
     this.childValueChanged.emit(value);
